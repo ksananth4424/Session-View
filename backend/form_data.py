9acd7cea7a011335029112c6829e500e4618bd4f
Pradyumn Kangule <123559917+Pradyumn1618@users.noreply.github.com>
2023-09-27 02:47:34 +0530

    Add files via upload

--- backend/form_data.py
+++ backend/form_data.py
@@ -3,15 +3,18 @@ from fastapi import FastAPI, File, UploadFile
 from fastapi.responses import FileResponse
 import asyncio
 from typing import List
-from firebase_admin import firestore
+import firebase_admin
+from firebase_admin import credentials,firestore
 from pydantic import BaseModel
 from . import deploy
+cred = credentials.Certificate('hackathon2-4d029-firebase-adminsdk-ckc2q-6771c29c2a.json')
+firebase_admin.initialize_app(cred)
 
 class Form_data(BaseModel):
     review:str
 
 clubs={
-    "club1":{
+    "club1":{'counter':0,
          'Advanced':0,'Bad':0,'Bad Management':0,'Beginner Friendly':0,'Overall Good':0,'Good Management':0,'Good Resources':0,'Informative and Knowledgeable':0,'Lengthy':0,'Short':0
     },
     "club2":{}
@@ -32,19 +35,21 @@ form_data_queue = asyncio.Queue()
 async def submit_form(request: Request, form_data: Form_data,session:session_data):
     
         tags=deploy.predict(form_data.review)
+        clubs[session.club_id]['counter']=clubs[session.club_id]['counter']+1
+        print(tags)
         #pass this list to ml model u will get result
-        # update(result,session)
         for sublist in tags:
-             if sublist[1]>0.1:
-                clubs[session.club_id][sublist[0]] = clubs[session.club_id][sublist[0]]+1
+                clubs[session.club_id][sublist[0]] = (clubs[session.club_id][sublist[0]]*(clubs[session.club_id]['counter']-1)+sublist[1])/clubs[session.club_id]['counter']
 
+        update(clubs[session.club_id],session)
         return clubs[session.club_id]
 
     
 
 @router.post("/stop")
 async def toggle_form_submission(club_id:str):
-    clubs[club_id].clear()
+    for key in clubs[club_id].keys():
+         clubs[club_id][key]=0
 
 db=firestore.client()
 def update(result:dict,session:session_data):
