28045bef2cd4e114e98ab7107e619c21204c65d0
ksananth4424 <ksananth4424@gmail.com>
2023-09-26 22:51:19 +0530

    backend initialization

--- data/script.py
+++ data/script.py
@@ -7,12 +7,23 @@ def generate(strings):
         combinations.extend(itertools.combinations(strings,r))
     return combinations
 
+# matrix = [
+#     ["good", "great", "bad", "worst", "average", "fine"],
+#     ["good management", "bad management"],
+#     ["resources", "no resources"],
+#     ["fast", "quick", "lengthy", "perfect time"],
+#     ["informative", "not informative"],
+#     ["advanced", "begineer friendly"]
+# ]
+
 input_strings = ["good", "great", "bad", "lengthy", "quick", "fine", "okay", "average", "informative", "resources", "no resources", "advanced", "begineer friendly", "good", "good management", "bad management", "short", "knowledeable"]
 combinations = generate(input_strings)
 
-file = "data/review_tags.csv"
+# combinations = list(itertools.product(*matrix))
+
+file = "data/review_tags1.csv"
 i=0
 with open(file, "w", newline='') as file:
     writer = csv.writer(file)
     for i in range(0, len(combinations)):
-        writer.writerow(combinations[i])    
+        writer.writerow(combinations[i])
