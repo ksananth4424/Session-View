2b64e6de026ee3fd5971ef9d4bb79d0b98ef9f56
Pradyumn Kangule <123559917+Pradyumn1618@users.noreply.github.com>
2023-09-27 19:41:28 +0530

    Add files via upload

--- backend/form_data.py
+++ backend/form_data.py
@@ -1,28 +1,24 @@
-from fastapi import APIRouter,HTTPException,Request,Response,status
-from fastapi import FastAPI, File, UploadFile
-from fastapi.responses import FileResponse
+from fastapi import APIRouter,HTTPException,status
 import asyncio
 from typing import List
 import firebase_admin
 from firebase_admin import credentials,firestore
 from pydantic import BaseModel
 from . import deploy
-cred = credentials.Certificate('hackathon2-4d029-firebase-adminsdk-ckc2q-6771c29c2a.json')
+cred = credentials.Certificate('hackathon-9f5f7-firebase-adminsdk-1vfic-eb2298f178.json')
 firebase_admin.initialize_app(cred)
 
 class Form_data(BaseModel):
     review:str
 
-clubs={
-    "club1":{'counter':0,
-         'Advanced':0,'Bad':0,'Bad Management':0,'Beginner Friendly':0,'Overall Good':0,'Good Management':0,'Good Resources':0,'Informative and Knowledgeable':0,'Lengthy':0,'Short':0
-    },
-    "club2":{}
-}
-
-
+clubs={}
 
 class session_data(BaseModel):
+    club_id:str
+    session_id:str
+    review_id:str
+
+class session(BaseModel):
     club_id:str
     session_id:str
 
@@ -30,28 +26,41 @@ router=APIRouter()
 
 form_data_queue = asyncio.Queue()  
 
+db=firestore.client()
 
 @router.post("/submit_form")
-async def submit_form(request: Request, form_data: Form_data,session:session_data):
-    
-        tags=deploy.predict(form_data.review)
-        clubs[session.club_id]['counter']=clubs[session.club_id]['counter']+1
-        print(tags)
-        #pass this list to ml model u will get result
-        for sublist in tags:
-                clubs[session.club_id][sublist[0]] = (clubs[session.club_id][sublist[0]]*(clubs[session.club_id]['counter']-1)+sublist[1])/clubs[session.club_id]['counter']
+async def submit_form(session:session_data):
+        review=db.collection("clubs").document(session.club_id).collection("sessions").document(session.session_id).collection("reviews").document(session.review_id)
+        doc=review.get()
+        field=doc.to_dict().get("review")
+        tags=deploy.predict(field)
+        if session.club_id not in clubs:
+             clubs[session.club_id]={}
 
-        update(clubs[session.club_id],session)
-        return clubs[session.club_id]
+        if session.session_id not in clubs[session.club_id]:
+            clubs[session.club_id][session.session_id] = {'counter': 0}
+
+            clubs[session.club_id][session.session_id]['counter'] += 1
+
+   
+        for sublist in tags:
+            tag_name = sublist[0]
+            tag_value = sublist[1]
+            clubs[session.club_id][session.session_id].setdefault(tag_name, 0)
+            if tag_value>0.1:
+                 clubs[session.club_id][session.session_id][sublist[0]]=clubs[session.club_id][session.session_id][sublist[0]]+1
+        
+
+        update(clubs[session.club_id][session.session_id],session)
+        return clubs[session.club_id][session.session_id]
 
     
 
 @router.post("/stop")
-async def toggle_form_submission(club_id:str):
-    for key in clubs[club_id].keys():
-         clubs[club_id][key]=0
+async def toggle_form_submission(session:session):
+    stop={"state":2}
+    db.collection("clubs").document(session.club_id).collection("sessions").document(session.session_id).set(stop,merge=True)
 
-db=firestore.client()
 def update(result:dict,session:session_data):
     try:
         doc=db.collection("clubs").document(session.club_id).collection("sessions").document(session.session_id)
